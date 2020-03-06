f79b106da884e20aee4132935d7f657c1ce05f6b
Barbra Mootian <bmootian@gmail.com>
2020-03-06 17:38:23 +0300

    This is our  second javaScript writing in Html

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -3,3 +3,26 @@
     console.log("my Age",myAge);
     var message1 ="Hello!" + myAge;
     console.log("message1=", message1);
+    //create variableto store infromation
+    var variable1 = 25;
+    variable1 = variable1 + 1;
+    console.log(variable1);
+
+    //Run functions
+
+function doStuff(name, age, thingIlove) {
+        console.log("My name is " + name); 
+        console.log("My age is " + age); 
+        console.log("I love " + thingIlove); 
+    }
+    doStuff("Barbra", 20, "Listening to music!");
+    doStuff("Sylvia", 18, "Dancing!");
+
+   // Array (or a list)
+   var namesOfStudents = ["Barbra" , "Sylvia" ];
+   var ageOfStudents = [ 20 ,18];
+   var thingsStudentslove = ["Listening to music" ,"Dancing"];
+   var theFoodStudentslove =["chip chicken" ,"Chicken sizliar"];
+   var gameStudentslikes =["football" , "basketball"];
+   console.log(namesOfStudents ,ageOfStudents ,thingsStudentslove ,theFoodStudentslove , gameStudentslikes );
+   
